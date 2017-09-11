225b6a6569078e808da9136768b2bd7f2fb9a9b7
Bobby Earl <bobby@simplyearl.com>
2017-09-11 19:16:59 -0400

    Added publish functionality

---
+++ scripts/publish.sh
@@ -0,0 +1,25 @@
+# Fail the build if this step fails
+set -e
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+
+  # Setup git and clone our repo
+  git config --global user.email "bobby@simplyearl.com"
+  git config --global user.name "Bobby Earl"
+  git clone --quiet https://${GITHUB_PAT}@github.com/bobbyearl/traffic-dist.git > /dev/null
+
+  # Copy our "built" files into our clone and add them
+  cp -a dist/. traffic-dist/
+  cd traffic-dist
+  git add .
+
+  # Make sure there are any changes.
+  # git commit will fail if there are no changes
+  if [ -z "$(git status --porcelain)" ]; then
+    echo -e "No changes to commit\n."
+  else
+    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to master. [ci skip]"
+    git push -fq origin master > /dev/null
+    echo -e "Successfully updated.\n"
+  fi
+fi
