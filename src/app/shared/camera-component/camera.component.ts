b75b2067d6a88f8d51df1cf1f5441e3a36191141
Bobby Earl <bobby.earl@blackbaud.com>
2018-08-28 00:09:10 -0400

    Saving WIP

--- src/app/shared/camera-component/camera.component.ts
+++ src/app/shared/camera-component/camera.component.ts
@@ -1,58 +1,73 @@
 import {
   Component,
-  ElementRef,
   Input,
   AfterViewInit,
   OnDestroy
 } from '@angular/core';
 
-import { SkyAppAssetsService } from '@blackbaud/skyux-builder/runtime/assets.service';
+import {
+  SkyAppConfig
+} from '@blackbaud/skyux-builder/runtime';
 
-declare var jwplayer: any;
+import * as HLS from 'hls.js';
+import * as URI from 'urijs';
+
+let cameraCounter = 0;
 
 @Component({
   selector: 'be-camera',
-  templateUrl: './camera.component.html'
+  templateUrl: './camera.component.html',
+  styleUrls: [ './be-camera.component.scss' ]
 })
 export class CameraComponent implements AfterViewInit, OnDestroy {
 
   @Input()
   public feature: any;
 
-  private flashplayer: string;
-  private _player: any;
+  private _player: HLS;
 
-  public get player(): any {
-    this._player = this._player || jwplayer(this.elementRef.nativeElement);
+  public get player(): HLS {
+    this._player = this._player || new HLS();
     return this._player;
   }
 
+  public id: string = 'cam-' + cameraCounter++;
+
   constructor(
-    private elementRef: ElementRef,
-    private assets: SkyAppAssetsService
-  ) {
-    this.flashplayer = this.assets.getUrl('camera-overlay-js/jwplayer.flash.swf');
-  }
+    private skyAppConfig: SkyAppConfig
+  ) { }
 
   public ngAfterViewInit() {
-    this.player.setup({
-      flashplayer: this.flashplayer,
-      controls: false,
-      aspectratio: '4:3',
-      width: '100%',
-      stretching: 'fill',
-      playlist: [{
-        sources: [
-          { file: this.feature.properties.rtmp_url },
-          { file: this.feature.properties.http_url }
-        ]
-      }]
-    }).on('ready', function() {
-      this.play();
+    const video = <HTMLVideoElement>document.querySelector(`#${this.id}`);
+    const uri = new URI(this.feature.properties.http_url);
+
+    if (this.skyAppConfig.skyux.appSettings['useProxy']) {
+      const origin = uri.origin();
+      const directory = uri.directory() + '/';
+      uri.origin(this.skyAppConfig.skyux.appSettings['proxy']);
+      uri.setQuery('origin', origin);
+      uri.setQuery('directory', directory);
+    }
+
+    if (HLS.isSupported()) {
+      this.player.loadSource(uri.toString());
+      this.player.attachMedia(video);
+    } else {
+      console.log('Not supported.  :-(');
+    }
+
+    this.player.on(HLS.Events.MANIFEST_PARSED, () => {
+      video.play();
+    });
+
+    this.player.on(HLS.Events.ERROR, (e: any, data: any) => {
+      console.error(data);
+      // console.error(e);
     });
   }
 
   public ngOnDestroy() {
-    this.player.remove();
+    this.player.detachMedia();
+    this.player.destroy();
   }
 }
